4c299bae043ae85d3aecc1300ea3d13d2bfee389
Uche Chilaka <uche@lar.city>
2025-04-16 09:28:38 -0400

    refactor: composition of tailwind.config.js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,28 +3,33 @@
 // // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-assignment
 // const defaultTheme = require("tailwindcss/defaultTheme");
 
-export default {
-  content: [
-    "./public/*.html",
-    "./node_modules/flowbite/**/*.js",
-    "./app/helpers/**/*.rb",
-    "./app/views/**/*.{html,html.erb,erb}",
-    "./app/views/devise/**/*.{html,html.erb,erb}",
-    "./app/frontend/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      // fontFamily: {
-      //   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-      //   sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      // },
-    },
+export const content = [
+  "./public/*.html",
+  "./node_modules/flowbite/**/*.js",
+  "./app/helpers/**/*.rb",
+  "./app/views/**/*.{html,html.erb,erb}",
+  "./app/views/devise/**/*.{html,html.erb,erb}",
+];
+
+export const theme = {
+  extend: {
+    // fontFamily: {
+    //   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    //   sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+    // },
   },
-  plugins: [
-    "flowbite/plugin",
-    "@tailwindcss/forms",
-    "@tailwindcss/aspect-ratio",
-    "@tailwindcss/typography",
-    "@tailwindcss/container-queries",
-  ],
+};
+
+export const plugins = [
+  "flowbite/plugin",
+  "@tailwindcss/forms",
+  "@tailwindcss/aspect-ratio",
+  "@tailwindcss/typography",
+  "@tailwindcss/container-queries",
+];
+
+export default {
+  content,
+  theme,
+  plugins,
 };
