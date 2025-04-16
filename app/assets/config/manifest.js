50ddb4c633ff6bd5ddc1fed43ea83fab95ca915b
Uche Chilaka <uche@lar.city>
2025-04-16 08:56:00 -0400

    chore: SAVEPOINT

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,7 @@
 //= link_tree ../builds
 //= link_directory ../../../node_modules/tailwindcss .css
 //= link application.css
+// TODO: we shouldn't need this here - try using dynamic style link tags instead
 //= link tailwind.css
+// TODO: we shouldn't need this here - try using dynamic style link tags instead
 //= link actiontext.css
