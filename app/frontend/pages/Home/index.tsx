66985655ca9af717098f4269cd532868f36b0115
Uche Chilaka <uche@lar.city>
2025-05-03 04:39:07 -0400

    refactor: typed animation as callback

--- app/frontend/pages/Home/index.tsx
+++ app/frontend/pages/Home/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import Typed from "typed.js";
 import DemoLayout from "@/components/DemoLayout";
 import { FCWithLayout } from "@/@types";
 import BackgroundVideo from "./BackgroundVideo";
 
 const Home: FCWithLayout = () => {
-  const el = useRef<HTMLHeadingElement>(null);
+  const autoCursorEl = useRef<HTMLHeadingElement>(null);
   const typed = useRef<Typed | null>(null);
 
   const onStringTyped = (arrayPos: number, typist: Typed) => {
@@ -18,9 +18,9 @@ const Home: FCWithLayout = () => {
     setTimeout(() => typist.start(), arrayPos == 2 ? 5000 : 2500);
   };
 
-  useEffect(() => {
-    if (el.current) {
-      typed.current = new Typed(el.current, {
+  const initTypedAnimation = useCallback(
+    (target: HTMLHeadingElement): Typed => {
+      return new Typed(target, {
         strings: [
           // Discover the tools that will help you work smarter, not harder
           "Tech",
@@ -35,29 +35,37 @@ const Home: FCWithLayout = () => {
         loopCount: 5,
         onStringTyped,
       });
-    }
+    },
+    []
+  );
+
+  // Initialize Typed.js animation
+  useEffect(() => {
+    if (autoCursorEl.current)
+      typed.current = initTypedAnimation(autoCursorEl.current);
 
     return () => {
       typed.current?.destroy();
     };
   }, []);
 
+  // Initialize blur on scroll effect
+  useEffect(() => {}, []);
+
   return (
     <>
       <BackgroundVideo />
       <div className="relative isolate z-30 px-6 pt-[22rem] sm:pt-[36rem] lg:px-8 min-h-[var(--min-h-with-footer-peek)]">
+        {/* Top Blur Shape */}
         <div
+          id="scroll-blur-element"
           aria-hidden="true"
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
         >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          />
+          <div className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] blur-shape" />
         </div>
+
+        {/* Content Section */}
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -72,7 +80,7 @@ const Home: FCWithLayout = () => {
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
               Level Up Your
               <br />
-              <span ref={el} />
+              <span ref={autoCursorEl} />
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
               Need to scale up your online business, automate digital marketing,
