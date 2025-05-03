826a565f09671635d0a09e403b97fd83f271ba24
Uche Chilaka <uche@lar.city>
2025-05-03 12:05:06 -0400

    feat: (WIP) AnonymousUser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_19_114933) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_03_155807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -96,6 +96,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_19_114933) do
     t.string "providers", default: [], array: true
     t.jsonb "uids", default: {}
     t.string "nickname"
+    t.string "type", default: "User", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
