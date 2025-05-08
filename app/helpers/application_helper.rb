a4c2fab5d46ac2e5ac3c7266f17386a43228d237
Uchenna Chilaka <uche@lar.city>
2025-05-07 23:40:21 -0400

    refactor: breakdown NavbarHelper to Navigable modules

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,7 @@
 
 module ApplicationHelper
   include StyleHelper
-
-  def anonymous_dom_id(seed_string)
-    "#{slugify_for_dom(seed_string)}--#{SecureRandom.hex(4)}"
-  end
-
-  def slugify_for_dom(name)
-    name.to_s.parameterize(separator: '-')
-  end
+  include DynamicDomElementCapable
 
   def modal_dom_id(resource, content_type: nil)
     raise ArgumentError, "#{resource.class.name} MUST be Renderable" \
