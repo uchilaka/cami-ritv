2235093648c4dddada8c8d44dc78d45d3d6c6390
Uchenna Chilaka <uche@lar.city>
2025-06-11 05:57:01 -0400

    refactor: (WIP) webhook_policy

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,31 +1,31 @@
+# frozen_string_literal: true
+
 class WebhooksController < ApplicationController
-  before_action :set_webhook, only: %i[ show edit update destroy ]
+  before_action :set_webhook, only: %i[show edit update destroy]
 
   # GET /webhooks or /webhooks.json
   def index
-    @webhooks = Webhook.all
+    @webhooks = policy_scope(Webhook)
   end
 
   # GET /webhooks/1 or /webhooks/1.json
-  def show
-  end
+  def show; end
 
   # GET /webhooks/new
   def new
-    @webhook = Webhook.new
+    @webhook = authorize Webhook.new
   end
 
   # GET /webhooks/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /webhooks or /webhooks.json
   def create
-    @webhook = Webhook.new(webhook_params)
+    @webhook = authorize Webhook.new(webhook_params)
 
     respond_to do |format|
       if @webhook.save
-        format.html { redirect_to @webhook, notice: "Webhook was successfully created." }
+        format.html { redirect_to @webhook, notice: 'Webhook was successfully created.' }
         format.json { render :show, status: :created, location: @webhook }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class WebhooksController < ApplicationController
   def update
     respond_to do |format|
       if @webhook.update(webhook_params)
-        format.html { redirect_to @webhook, notice: "Webhook was successfully updated." }
+        format.html { redirect_to @webhook, notice: 'Webhook was successfully updated.' }
         format.json { render :show, status: :ok, location: @webhook }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +52,20 @@ class WebhooksController < ApplicationController
     @webhook.destroy!
 
     respond_to do |format|
-      format.html { redirect_to webhooks_path, status: :see_other, notice: "Webhook was successfully destroyed." }
+      format.html { redirect_to webhooks_path, status: :see_other, notice: 'Webhook was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_webhook
-      @webhook = Webhook.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def webhook_params
-      params.expect(webhook: [ :url, :verification_token ])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_webhook
+    @webhook = authorize Webhook.find(params.expect(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+  def webhook_params
+    params.expect(webhook: %i[url verification_token])
+  end
 end
--- spec/models/webhook_spec.rb
+++ spec/models/webhook_spec.rb
@@ -11,5 +11,29 @@
 require 'rails_helper'
 
 RSpec.describe Webhook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:webhook) { described_class.new(url:, verification_token:) }
+
+  let(:url) { 'https://example.com/webhook' }
+  let(:verification_token) { 'secret-token' }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:url) }
+    it { is_expected.to validate_presence_of(:verification_token) }
+
+    context 'with invalid URL' do
+      let(:url) { 'invalid-url' }
+      let(:error_message) { I18n.t('validators.errors.invalid_url', value: url) }
+
+      before { webhook.valid? }
+
+      it { is_expected.not_to be_valid }
+      it { expect(webhook.errors[:url]).to include(error_message) }
+    end
+
+    context 'with valid URL' do
+      let(:url) { 'https://valid-url.com' }
+
+      it { is_expected.to be_valid }
+    end
+  end
 end
