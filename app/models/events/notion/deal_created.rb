daef093e4ed98b3be814aa0a1271331631e66b10
Uchenna Chilaka <uche@lar.city>
2025-06-14 05:09:39 -0400

    refactor: (WIP) +Notion::DealCreated event

---
+++ app/models/events/notion/deal_created.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: generic_events
+#
+#  id             :uuid             not null, primary key
+#  eventable_type :string
+#  type           :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  eventable_id   :uuid
+#  metadatum_id   :uuid
+#
+# Indexes
+#
+#  index_generic_events_on_eventable     (eventable_type,eventable_id)
+#  index_generic_events_on_metadatum_id  (metadatum_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (metadatum_id => metadata.id)
+#
+module Notion
+  class DealCreated < GenericEvent
+    has_one :metadatum, required: false, dependent: :destroy
+  end
+end
