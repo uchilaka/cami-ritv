30335f5bcb7eb9f8848ff4f39d1e9acf2529b245
Uchenna Chilaka <uche@lar.city>
2025-06-14 14:00:29 -0400

    refactor: smol cleanup

--- db/migrate/20250614172751_add_appendable_to_metadata.rb
+++ db/migrate/20250614172751_add_appendable_to_metadata.rb
@@ -5,8 +5,6 @@ class AddAppendableToMetadata < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    # add_column :metadata, :appendable_id, :string
-    # add_column :metadata, :appendable_type, :string
     add_reference :metadata,
                   :appendable,
                   polymorphic: true, type: :uuid,
