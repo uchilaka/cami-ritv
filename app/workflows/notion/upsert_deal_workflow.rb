37231c84bd40f39bb4fa1d64955679aaa9c88405
Uchenna Chilaka <uche@lar.city>
2025-06-14 09:05:46 -0400

    refactor: a bit o' streamlining

--- app/workflows/notion/upsert_deal_workflow.rb
+++ app/workflows/notion/upsert_deal_workflow.rb
@@ -9,28 +9,29 @@ module Notion
       context.database = context.event.parent
       context.remote_record_id = context.entity.id
 
-      case context.event.type
-      when 'page.created'
-        context.system_event = ::Notion::DealCreatedEvent.new(
-          metadatum: {
-            remote_record_id: context.remote_record_id,
-            database: context.database.serializable_hash,
-            **context.event.serializable_hash,
-          }
-        )
-      when 'page.properties_updated'
-        context.system_event = ::Notion::DealUpdatedEvent.new(
-          metadatum: {
-            remote_record_id: context.remote_record_id,
-            database: context.database.serializable_hash,
-            **context.event.serializable_hash,
-          }
-        )
-      else
-        error =
-          I18n.t('workflows.upsert_deal_workflow.errors.unsupported_event_type', event_type: context.event.type)
-        fail!(error:)
-      end
+      context.system_event =
+        case context.event.type
+        when 'page.created'
+          ::Notion::DealCreatedEvent.new(
+            metadatum: {
+              remote_record_id: context.remote_record_id,
+              database: context.database.serializable_hash,
+              **context.event.serializable_hash,
+            }
+          )
+        when 'page.properties_updated'
+          ::Notion::DealUpdatedEvent.new(
+            metadatum: {
+              remote_record_id: context.remote_record_id,
+              database: context.database.serializable_hash,
+              **context.event.serializable_hash,
+            }
+          )
+        else
+          error =
+            I18n.t('workflows.upsert_deal_workflow.errors.unsupported_event_type', event_type: context.event.type)
+          fail!(error:)
+        end
     ensure
       remote_event_id = context.event.id
       status = context.success? ? 'success' : 'failure'
