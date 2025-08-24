cb59dd620db25519a0cd24201b25bf1cd2d5e993
Uchenna Chilaka <uche@lar.city>
2025-08-24 05:11:25 -0400

    chore: SAVEPOINT

--- app/helpers/webhooks/records_helper.rb
+++ app/helpers/webhooks/records_helper.rb
@@ -2,22 +2,26 @@
 
 module Webhooks
   module RecordsHelper
-    def record_view_partial_path
+    def webhook_record_view_partial_path
       case @webhook&.slug
       when 'notion'
-        "webhooks/#{@webhook.slug}/#{record_view_name_from_action}"
+        "webhooks/#{@webhook.slug}/#{webhook_record_view_name_from_action}"
       else
-        "webhooks/records/#{record_view_name_from_action}"
+        "webhooks/records/#{webhook_record_view_name_from_action}"
       end
     end
 
-    def record_view_name_from_action
+    def webhook_record_view_name_from_action
       case action_name
       when 'new', 'create', 'edit', 'update'
         'form'
       else
         'record'
       end
+    end
+
+    def notion_deals_pagination_enabled?
+      @notion_deals_pagination_enabled ||= Flipper.enabled? :feat__notion_deals_pagination, current_user
     end
   end
 end
