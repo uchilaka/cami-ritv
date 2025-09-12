18121e109957281366a07fd51f33053562cef25a
Uchenna Chilaka <uche@lar.city>
2025-09-12 10:36:00 -0400

    chore: SAVEPOINT

--- app/jobs/digital_ocean/delete_domain_record_job.rb
+++ app/jobs/digital_ocean/delete_domain_record_job.rb
@@ -1,10 +1,34 @@
 # frozen_string_literal: true
 
+# require 'active_job'
+
 module DigitalOcean
   class DeleteDomainRecordJob < ApplicationJob
     queue_as :yeet
 
     def perform(id, domain:, access_token: nil, pretend: false)
+      # Check if there's a pending failed execution
+
+      # pending_failed_jobs = SolidQueue::Job.where(
+      #   "arguments ->> 0 = ? AND failed_at IS NOT NULL AND retry_count < max_retries",
+      #   id.to_s
+      # )
+
+      # Use Arel to get table name for SolidQueue::FailedExecution
+      # failed_executions_table = SolidQueue::FailedExecution.arel_table
+
+      # Query solid queue jobs for any jobs with the same first argument
+      # value (id) that have any SolidQueue::FailedExecution records associated
+      # with them. This is to avoid duplicate jobs being enqueued if a previous
+      # job failed and is pending retry.
+      jobs_schema = SolidQueue::Job.arel_table.name.to_s.to_sym
+      failed_execution_schema = SolidQueue::FailedExecution.arel_table.name.to_s.to_sym
+      failed_jobs = SolidQueue::Job.joins(
+        "INNER JOIN #{failed_execution_schema} ON #{jobs_schema}.id = #{failed_execution_schema}.job_id"
+      ).where('arguments ->> 0 = ?', id.to_s)
+
+
+      # SolidQueue::FailedExecution
       DigitalOcean::API.delete_domain_record(id, domain:, access_token:, pretend:)
     end
 
