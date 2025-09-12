3d960a93171eb75f2388188a7362697da5520401
Uchenna Chilaka <uche@lar.city>
2025-09-12 08:56:18 -0400

    fix: directory name for DigitalOcean::DeleteDomainRecordJob

--- lib/digital_ocean.rb
+++ lib/digital_ocean.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 require 'digital_ocean/api'
-require 'jobs/digitalocean/delete_domain_record_job'
+require 'jobs/digital_ocean/delete_domain_record_job'
 
 module DigitalOcean; end
