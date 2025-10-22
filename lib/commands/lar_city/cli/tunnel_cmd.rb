18901dec19bdf78fe04dddd5840a17d172251216
Uchenna Chilaka <uche@lar.city>
2025-10-22 03:36:13 -0400

    refactor: (WIP) update specs

--- lib/commands/lar_city/cli/tunnel_cmd.rb
+++ lib/commands/lar_city/cli/tunnel_cmd.rb
@@ -59,12 +59,10 @@ module LarCity
           open_via_ngrok
         else
           error_msg = <<~ERROR
-            ************************************************************************
-            *  ERROR: proxy tunnels can only be opened in supported environments.  *
-            ************************************************************************
+            Unsupported environment: #{detected_environment}. A proxy tunnel \
+            can only be opened in the development or staging environments.
           ERROR
           say_error error_msg
-          say_error "Unsupported environment: #{detected_environment}"
         end
       end
 
