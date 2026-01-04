fb9c42fe1ac67a75486865675815567991f6301c
uche-at-abt <uchilaka@actbluetech.com>
2026-01-04 02:19:25 -1000

    refactor: notes about Twenty CRM ENV variables

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -19,7 +19,18 @@ required_env_vars =
 unless Rails.env.test?
   case Rails.env
   when 'development'
-    required_env_vars += %w[CRM_SERVICE_PORT]
+    required_env_vars += [
+      # Random generated secret for Twenty CRM in development environment
+      'APP_SECRET',
+      # Service port for Twenty CRM in development environment
+      'CRM_SERVICE_PORT',
+      'CRM_REDIS_URL',
+      'CRM_DATABASE_NAME',
+      # The internal service URL for accessing the Twenty CRM app/admin
+      'SERVICE_URL',
+      # The public-accessible proxy URL for accessing the Twenty CRM app/admin
+      'PUBLIC_DOMAIN_URL'
+    ]
   else
     # Assumes production-like environment
     required_env_vars += production_env_vars
