06887a8873fa2af8d66bdbe3c2add2ae5a97d99d
uche-at-abt <uchilaka@actbluetech.com>
2026-01-04 02:06:09 -1000

    refactor: pull forward output_helpers_spec from #245

--- spec/lib/commands/lar_city/cli/output_helpers_spec.rb
+++ spec/lib/commands/lar_city/cli/output_helpers_spec.rb
@@ -5,7 +5,7 @@ require 'lar_city/cli/output_helpers'
 require 'thor'
 
 RSpec.describe LarCity::CLI::OutputHelpers, :time_sensitive do
-  # Define inline Thor class for testing
+  # Inline Thor class used for testing OutputHelpers behavior
   TestThorClass = Class.new(Thor) do
     include LarCity::CLI::OutputHelpers
 
@@ -24,6 +24,50 @@ RSpec.describe LarCity::CLI::OutputHelpers, :time_sensitive do
   end
 
   subject(:cmd) { TestThorClass.new }
+
+  describe "#pretend?" do
+    subject(:cmd) { TestThorClass.new(cmd_args, **options) }
+
+    it("is a protected method") { expect(cmd.protected_methods).to include(:pretend?) }
+
+    let(:result) { cmd.send(:pretend?) }
+    let(:cmd_args) { [] }
+    let(:options) { {} }
+
+    context "when dry_run is enabled" do
+      let(:options) { { dry_run: true } }
+
+      it { expect(result).to be true }
+    end
+
+    context "when dry_run is disabled" do
+      let(:options) { { dry_run: false } }
+
+      it { expect(result).to be false }
+    end
+  end
+
+  describe "#debug?" do
+    subject(:cmd) { TestThorClass.new(cmd_args, **options) }
+
+    it("is a protected method") { expect(cmd.protected_methods).to include(:debug?) }
+
+    let(:result) { cmd.send(:debug?) }
+    let(:cmd_args) { [] }
+    let(:options) { {} }
+
+    context "when verbose is enabled" do
+      let(:options) { { verbose: true } }
+
+      it { expect(result).to be true }
+    end
+
+    context "when verbose is disabled" do
+      let(:options) { { verbose: false } }
+
+      it { expect(result).to be false }
+    end
+  end
 
   describe '#partially_masked_secret' do
     shared_examples 'expected secret masking' do |input, expected_output|
@@ -69,6 +113,52 @@ RSpec.describe LarCity::CLI::OutputHelpers, :time_sensitive do
       let(:filename) { 'random_file_name.txt' }
 
       it { is_expected.to be_nil }
+    end
+  end
+
+  describe "#tally" do
+    subject(:result) { cmd.tally(set, collection_class.name) }
+    let(:collection_class) { Webhook }
+
+    context "with no items" do
+      let(:set) { Webhook.none }
+
+      it { is_expected.to eq "0 Webhooks" }
+    end
+
+    context "with a single item" do
+      let(:set) { Fabricate.times(1, :webhook) }
+
+      it { is_expected.to eq "1 Webhook" }
+    end
+
+    context "with multiple items" do
+      let(:set) { Fabricate.times(3, :webhook) }
+
+      it { is_expected.to eq "3 Webhooks" }
+    end
+  end
+
+  describe "#range" do
+    subject(:result) { cmd.range(set) }
+
+    context "with no items" do
+      let(:set) { Webhook.none }
+
+      it { is_expected.to be_nil }
+    end
+
+    context "with a single item" do
+      let(:set) { Fabricate.times(1, :webhook) }
+
+      it { is_expected.to eq "[1]" }
+    end
+
+    context "with multiple items" do
+      let(:set) { Fabricate.times(3, :webhook) }
+      let(:ids) { set.pluck(:id).sort }
+
+      it { is_expected.to eq "[1-3]" }
     end
   end
 end
