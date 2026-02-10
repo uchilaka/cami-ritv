b3b839192689bfe4bc5fa89569ee92492d604305
Uchenna Chilaka <uche@lar.city>
2026-02-10 03:07:01 -0500

    fix: zeitwerk error

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class UrlValidator < ActiveModel::EachValidator
+class URLValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
