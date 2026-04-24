a5ae1d49e915286fad95eefe55dccd5d1d4f58eb
Uchenna Chilaka <uche@lar.city>
2026-04-24 17:42:42 -0400

    fix: backend.tf should be hard coded only

--- .terraform/backend.tf
+++ .terraform/backend.tf
@@ -1,15 +1,15 @@
 terraform {
   backend "s3" {
     # Replace with your bucket name
-    bucket    = var.tf_state_bucket
-    key       = "cami/fly/terraform.tfstate"
-    region    = "us-east-1"
+    # bucket    = "white-cloud-8496"
+    # key       = "cami/fly/terraform.tfstate"
+    # region    = "us-east-1"
 
     # For Tigris (Fly.io), you would set the endpoint and skip some AWS-specific checks
-    endpoint                      = var.fly_aws_endpoint_url_s3
-    # skip_credentials_validation = true
-    # skip_metadata_api_check     = true
-    # skip_region_validation      = true
-    # skip_s3_checksum            = true
+    # endpoint                      = "https://fly.storage.tigris.dev"
+    # skip_credentials_validation   = true
+    # skip_metadata_api_check       = true
+    # skip_region_validation        = true
+    # skip_s3_checksum              = true
   }
 }
