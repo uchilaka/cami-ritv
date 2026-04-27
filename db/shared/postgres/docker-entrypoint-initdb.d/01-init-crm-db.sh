27fc7ce99289dcab091ded75eee1c09ac9f58bef
Uchenna Chilaka <uche@lar.city>
2026-04-26 23:17:22 -0400

    refactor: (WIP) update database ENV variable names

--- db/shared/postgres/docker-entrypoint-initdb.d/01-init-crm-db.sh
+++ db/shared/postgres/docker-entrypoint-initdb.d/01-init-crm-db.sh
@@ -4,19 +4,19 @@ set -e
 # This script is intended to be run by the official Postgres Docker image
 # during the initial database creation.
 
-if [ -n "$CRM_DATABASE_NAME" ]; then
+if [ -n "$APP_DATABASE_NAME_CRM" ]; then
   # Check if the database already exists
-  DB_EXISTS=$(psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_database WHERE datname = '$CRM_DATABASE_NAME'")
+  DB_EXISTS=$(psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_database WHERE datname = '$APP_DATABASE_NAME_CRM'")
 
   if [ "$DB_EXISTS" != "1" ]; then
-    echo "Creating CRM database: $CRM_DATABASE_NAME"
+    echo "Creating CRM database: $APP_DATABASE_NAME_CRM"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-      CREATE DATABASE "$CRM_DATABASE_NAME";
-      GRANT ALL PRIVILEGES ON DATABASE "$CRM_DATABASE_NAME" TO "$POSTGRES_USER";
+      CREATE DATABASE "$APP_DATABASE_NAME_CRM";
+      GRANT ALL PRIVILEGES ON DATABASE "$APP_DATABASE_NAME_CRM" TO "$POSTGRES_USER";
 EOSQL
   else
-    echo "CRM database $CRM_DATABASE_NAME already exists. Skipping creation."
+    echo "CRM database $APP_DATABASE_NAME_CRM already exists. Skipping creation."
   fi
 else
-  echo "CRM_DATABASE_NAME is not set. Skipping secondary database creation."
+  echo "APP_DATABASE_NAME_CRM is not set. Skipping secondary database creation."
 fi
