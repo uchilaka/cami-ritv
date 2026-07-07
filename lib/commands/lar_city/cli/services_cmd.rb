7e693a42d49ec910887f891e7768df8b83b65179
Uchenna Chilaka <uche@lar.city>
2026-07-07 02:29:19 -0400

    refactor: a few updates and savepoint

--- lib/commands/lar_city/cli/services_cmd.rb
+++ lib/commands/lar_city/cli/services_cmd.rb
@@ -86,9 +86,13 @@ module LarCity
         puts "Error setting up daemon: #{e.message}"
       end
 
-      add_port_option(desc: I18n.t('commands.services.lookup.options.port.short_desc'))
       desc 'lookup', I18n.t('commands.services.lookup.short_desc')
       long_desc I18n.t('commands.services.lookup.long_desc')
+      add_port_option(desc: I18n.t('commands.services.lookup.options.port.short_desc'))
+      option :name,
+             type: :string,
+             desc: "A human-friendly name to filter the lookup results (e.g., 'node' or 'python')",
+             required: false
       def lookup
         # Listening TCP ports: sudo lsof -nP -iTCP -sTCP:LISTEN
         # Specific TCP port: sudo lsof -i tcp:<port-number>
