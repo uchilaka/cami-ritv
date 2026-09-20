1494074d92a9cbfaeb3823921fee115910cd8afd
Uchenna Chilaka <uche@lar.city>
2026-09-20 17:25:23 -0400

    refactor: (WIP) making available environments more consistent

--- config/environments/beta.rb
+++ config/environments/beta.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # Configure logging for the app's mail service.
   config.action_mailer.logger = Rails.logger
 
-  config.action_mailer.default_url_options = { host: ENV.fetch('HOSTNAME', 'accounts.staging.larcity.tech') }
+  config.action_mailer.default_url_options = { host: ENV.fetch('HOSTNAME') }
 
   # Print deprecation notices to the Rails logger.
   # config.active_support.deprecation = :log
